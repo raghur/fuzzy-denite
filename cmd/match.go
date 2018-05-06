8680d3850d502616f7d1c200b518ae3c305f4bd1
Raghu Rajagopalan <raghu.rajagopalan@gmail.com>
2018-05-06 14:19:15 +0530

    Initial commit

---
+++ cmd/match.go
@@ -0,0 +1,98 @@
+// Copyright © 2018 NAME HERE <EMAIL ADDRESS>
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package cmd
+
+import (
+	"fmt"
+	"github.com/sahilm/fuzzy"
+	log "github.com/sirupsen/logrus"
+	"github.com/spf13/cobra"
+	"io/ioutil"
+	"os"
+	"strings"
+)
+
+var pattern string
+var inputFile string
+var highlight bool
+
+// matchCmd represents the match command
+var matchCmd = &cobra.Command{
+	Use:   "match",
+	Short: "A brief description of your command",
+	Long: `A longer description that spans multiple lines and likely contains examples
+and usage of using your command. For example:
+
+Cobra is a CLI library for Go that empowers applications.
+This application is a tool to generate the needed files
+to quickly create a Cobra application.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		log.Debug("match called")
+		log.Debug(pattern)
+		log.Debug(inputFile)
+		var dataBytes []byte
+		var err error
+		if inputFile == "-" {
+			dataBytes, err = ioutil.ReadAll(os.Stdin)
+		} else {
+			dataBytes, err = ioutil.ReadFile(inputFile)
+		}
+		if err != nil {
+			panic(err)
+		}
+
+		data := strings.Split(string(dataBytes), "\n")
+		matches := fuzzy.Find(pattern, data)
+		for _, match := range matches {
+			if !highlight {
+				fmt.Println(match.Str)
+			} else {
+				var output strings.Builder
+				for i := 0; i < len(match.Str); i++ {
+					if contains(i, match.MatchedIndexes) {
+						output.WriteString(fmt.Sprintf("\033[1m%s\033[0m", string(match.Str[i])))
+					} else {
+						output.WriteString(string(match.Str[i]))
+					}
+				}
+				fmt.Println(output.String())
+			}
+		}
+	},
+}
+
+func contains(needle int, haystack []int) bool {
+	for _, i := range haystack {
+		if needle == i {
+			return true
+		}
+	}
+	return false
+}
+func init() {
+	RootCmd.AddCommand(matchCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// matchCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	matchCmd.Flags().StringVarP(&pattern, "pattern", "p", "", "pattern to match on")
+	matchCmd.MarkFlagRequired("pattern")
+	matchCmd.Flags().StringVarP(&inputFile, "input", "i", "-", "Input file with line separated input; '-' means use stdio")
+	matchCmd.Flags().BoolVarP(&highlight, "highlight", "l", true, "highlight matches")
+}
---
+++ cmd/root.go
@@ -0,0 +1,69 @@
+// Copyright © 2018 NAME HERE <EMAIL ADDRESS>
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package cmd
+
+import (
+	"fmt"
+	"os"
+
+	log "github.com/sirupsen/logrus"
+	"github.com/spf13/cobra"
+)
+
+var logLevel string
+
+// RootCmd represents the base command when called without any subcommands
+var RootCmd = &cobra.Command{
+	Use:   "fuzzy-denite",
+	Short: "A brief description of your application",
+	Long: `A longer description that spans multiple lines and likely contains
+examples and usage of using your application. For example:
+
+Cobra is a CLI library for Go that empowers applications.
+This application is a tool to generate the needed files
+to quickly create a Cobra application.`,
+	// Uncomment the following line if your bare application
+	// has an action associated with it:
+	//	Run: func(cmd *cobra.Command, args []string) { },
+	PersistentPreRunE: func(cmd *cobra.Command, args []string) error {
+		lvl, err := log.ParseLevel(logLevel)
+		if err != nil {
+			log.Errorf("unable to parse log level %v", err)
+			return err
+		}
+		log.Debug("Setting log level to %v", lvl)
+		log.SetLevel(lvl)
+		return nil
+	},
+}
+
+// Execute adds all child commands to the root command and sets flags appropriately.
+// This is called by main.main(). It only needs to happen once to the rootCmd.
+func Execute() {
+	if err := RootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
+
+func init() {
+
+	// Here you will define your flags and configuration settings.
+	// Cobra supports persistent flags, which, if defined here,
+	// will be global for your application.
+	RootCmd.PersistentFlags().StringVar(&logLevel, "log", "warning", "log level to set {debug, info, warning, error, fatal, panic}")
+	// Cobra also supports local flags, which will only run
+	// when this action is called directly.
+	// RootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
