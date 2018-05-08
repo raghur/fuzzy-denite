3c75457de15108ef670fa6c4dadae382fcecb317
Raghu Rajagopalan <raghu.rajagopalan@gmail.com>
2018-05-08 10:26:31 +0530

    feat(lru): use LRU cache

--- cmd/server.go
+++ cmd/server.go
@@ -23,14 +23,15 @@ import (
 
 var contexts map[string][]string
 var port string
+var size int
 
 // serverCmd represents the server command
 var serverCmd = &cobra.Command{
 	Use:   "server",
 	Short: "Start in server mode on the specified port",
 	Run: func(cmd *cobra.Command, args []string) {
-		http.HandleFunc("/search", lib.Search)
-		log.Info("starting")
+		lib.CreateServer(size)
+		log.Infof("starting on %s", port)
 		http.ListenAndServe("localhost:"+port, nil)
 	},
 }
@@ -47,4 +48,5 @@ func init() {
 	// Cobra supports local flags which will only run when this command
 	// is called directly, e.g.:
 	serverCmd.Flags().StringVarP(&port, "port", "p", "9009", "port to run the server on")
+	serverCmd.Flags().IntVarP(&size, "size", "s", 20, "Size of the cache")
 }
--- lib/server.go
+++ lib/server.go
@@ -6,18 +6,20 @@ import (
 	"net/http"
 	"strconv"
 
+	lru "github.com/hashicorp/golang-lru"
 	"github.com/hydrogen18/stalecucumber"
 	"github.com/sahilm/fuzzy"
 	log "github.com/sirupsen/logrus"
 )
 
-var contexts map[string][]string
+var contexts *lru.ARCCache
 
-func init() {
-	contexts = make(map[string][]string)
+func CreateServer(cacheSize int) {
+	contexts, _ = lru.NewARC(cacheSize)
+	http.HandleFunc("/search", search)
 }
 
-func Search(w http.ResponseWriter, r *http.Request) {
+func search(w http.ResponseWriter, r *http.Request) {
 	file, _, fileerr := r.FormFile("data")
 	cid := r.Form.Get("cid")
 	if cid == "" {
@@ -26,7 +28,7 @@ func Search(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("You must pass a cid param"))
 		return
 	}
-	_, ok := contexts[cid]
+	ok := contexts.Contains(cid)
 	if !ok && fileerr != nil {
 		log.Errorf("/search: Context %v does not exist and no data passed", cid)
 		w.WriteHeader(http.StatusBadRequest)
@@ -41,7 +43,7 @@ func Search(w http.ResponseWriter, r *http.Request) {
 			log.Errorf("Could not read pickled stream %v", err)
 			w.WriteHeader(http.StatusInternalServerError)
 		}
-		contexts[cid] = data
+		contexts.Add(cid, data)
 		log.Infof("Created new context %v of size %v", cid, len(data))
 	}
 	max := r.Form.Get("max")
@@ -60,7 +62,10 @@ func min(x, y int) int {
 }
 func findMatchesFromContext(cid, pattern string, max int, writer io.Writer) {
 	log.Infof("Searching for %v in context %v", pattern, cid)
-	matches := fuzzy.Find(pattern, contexts[cid])
+	v, _ := contexts.Get(cid)
+	data := v.([]string)
+
+	matches := fuzzy.Find(pattern, data)
 	if max == 0 {
 		max = len(matches)
 	}
@@ -74,7 +79,7 @@ func findMatchesFromContext(cid, pattern string, max int, writer io.Writer) {
 		arr[i] = m.Str
 	}
 	log.Debugf("number of matches: %v", len(matches))
-	log.Infof("Got %v items. %v matches for %v. Will return max: %v results", len(contexts[cid]), len(matches), pattern, max)
+	log.Infof("Got %v items. %v matches for %v. Will return max: %v results", len(data), len(matches), pattern, max)
 
 	p := stalecucumber.NewPickler(writer)
 	p.Pickle(arr)
