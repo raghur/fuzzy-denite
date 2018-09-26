5f661979614131ea140822bc780abc346e087ac0
Raghu Rajagopalan <raghu.rajagopalan@gmail.com>
2018-09-26 14:18:41 +0530

    added new impl without closures.
    still slower than the first impl

--- rplugin/python3/nim_fuzzy.nim
+++ rplugin/python3/nim_fuzzy.nim
@@ -3,11 +3,16 @@ import strutils
 import binaryheap
 import logging
 import strformat
+# import nimprof
 
-let L = newConsoleLogger(levelThreshold = logging.Level.lvlDebug)
+let L = newConsoleLogger(levelThreshold = logging.Level.lvlNone)
+# let L = newConsoleLogger(levelThreshold = logging.Level.lvlDebug)
 addHandler(L)
+const MAXCHARS = 10
+const MAXCHARCOUNT = 10
 
 const sep:string = "-/\\_. "
+
 template l(fmt: string) =
     when not defined(release):
         debug(&fmt)
@@ -16,6 +21,107 @@ type
         found:bool
         positions: seq[int]
         sepScore, clusterScore, camelCaseScore: int
+    MatchIndex = array[MAXCHARS, array[0..MAXCHARCOUNT, int]]
+
+# forward declaration
+proc scorer(x: Match, candidate:string, ispath:bool=true): int {.inline.}
+
+proc assignScore(s: string, m: var Match) =
+    let l = m.positions.len
+    # min possible for consecutive matches like [51, 52, 53, 54]
+    # = 51 * 4 + (4*3/2)
+    # = 210
+    m.clusterScore = -1 * (m.positions[0] * l + l * (l-1) div 2)
+    for i,v in m.positions:
+        if v == 0:
+            m.sepScore.inc
+            m.clusterScore.inc
+        else:
+            let
+                prevChar = s[v-1]
+                ch = s[v]
+            if prevChar in sep:
+                m.sepScore.inc
+            if prevChar.isLowerAscii and ch.isUpperAscii:
+                m.camelCaseScore.inc
+        m.clusterScore.inc(v)
+
+proc rfirst1(a: openarray[int], predicate: proc(x: int): bool, last:int = -1): tuple[f:bool, v:int] =
+    #[
+    Find first item matching predicate in a(last .. 0)
+    ]#
+    var s = last
+    if s == -1: s = a.high
+    for i in countdown(s, 0):
+        if predicate(a[i]):
+            return (true, a[i])
+    return (false, -1)
+
+proc greaterThan(x : int): (proc(x:int):bool) = 
+    return proc(y:int):bool = return y > x
+
+proc checkFullMatch(indexArr: MatchIndex, lq: string, pos: int, m: var Match) =
+    var start = pos
+    m.positions[0] = start
+    l "Checking for entire string match starting from {pos}"
+    m.found = true
+    for k in 1..lq.high:
+        let arrIdx = lq.find(lq[k])
+        l "looking for {q[k]} in {indexArr[arrIdx]}, greater than {start}"
+        let (found, v) = rfirst1(indexArr[arrIdx], greaterThan(start), indexArr[arrIdx][MAXCHARCOUNT] - 1)
+        if found:
+            l "found at: {v}"
+            start = v
+            m.positions[k] = v
+        else:
+            m.found = false
+            break
+
+proc matcher(q, s: string):Match =
+    var indexArr: MatchIndex
+    let
+        lq = q.toLowerAscii()
+        ls = s.toLowerAscii()
+
+    result.found = false
+    result.positions = newSeq[int](len(q))
+    l "looking for {q} in {s}"
+    for i in countdown(ls.high, 0):
+        let c = ls[i]
+        let pos = lq.find(c)
+        if pos != -1:
+            let nextPos = indexArr[pos][MAXCHARCOUNT]
+            indexArr[pos][nextPos] = i
+            indexArr[pos][MAXCHARS].inc
+            l "found {c} at {i}, indexArr[{pos}][{nextPos}]={indexArr[pos][nextPos]}"
+            if pos == 0:
+                # first char was found - so see if we found the entire string
+                # after index i
+                checkFullMatch(indexArr, lq, i, result)
+                if result.found:
+                    assignScore(s, result)
+                    l "match: {result}"
+                    return
+    l "no match: {result}"
+    return result
+
+iterator fuzzyMatches01(q: string, candidates: openarray[string], limit: int, ispath:bool = true):tuple[i:int, r:int] =
+    let findFirstN = true
+    var count = 0
+    var heap = newHeap[tuple[i:int, r:int]]() do (a, b: tuple[i:int, r:int]) -> int:
+        b.r - a.r
+    for i, s in candidates:
+        let m = matcher(q, s)
+        if m.found:
+            let rank = scorer(m, s, ispath)
+            heap.push((i, rank))
+            count.inc
+            if findFirstN and count == limit * 5:
+                break
+    count = 0
+    while count < limit and heap.size > 0:
+        yield  heap.pop
+        count.inc
 
 proc scorer(x: Match, candidate:string, ispath:bool=true): int =
     let lqry = len(x.positions)
@@ -145,7 +251,8 @@ iterator fuzzyMatches(query:string, candidates: openarray[string], limit: int, i
 proc scoreMatchesStr(query: string, candidates: openarray[string], limit: int, ispath:bool=true): seq[tuple[i:int, r:int]] {.exportpy.} =
     result = newSeq[tuple[i:int, r:int]](limit)
     var idx = 0
-    for m in fuzzyMatches(query, candidates, limit, ispath):
+    for m in fuzzyMatches01(query, candidates, limit, ispath):
+    # for m in fuzzyMatches(query, candidates, limit, ispath):
         result[idx] = m
         idx.inc
     result.setlen(idx)
